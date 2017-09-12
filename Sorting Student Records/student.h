4049ca08fce6dfd39d0e274184f80fc3b14255b0
Michael Colson <mbcolson@yahoo.com>
2017-09-11 17:34:48 -0700

    Create student.h

---
+++ Sorting Student Records/student.h
@@ -0,0 +1,16 @@
+#define MAXSTUDENT 30
+
+struct Student
+{
+  char name[32];
+  int score;
+  struct Student *pLess;
+  struct Student *pMore;
+};
+
+int readStudent(struct Student *pRecord);
+int cmpStudent(const struct Student *pA, const struct Student *pB);
+void writeStudent(const struct Student *pRecord);
+void swapStudent(struct Student *pA, struct Student *pB);
+int findMinIndex(struct Student *pClass, int numStudent);
+void selSort(struct Student *pClass, int numStudent);
