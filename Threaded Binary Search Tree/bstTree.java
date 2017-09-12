f742ab278f8de605d0853d71f8f6cbba7d99a384
Michael Colson <mbcolson@yahoo.com>
2017-09-11 18:09:11 -0700

    Update bstTree.java

--- Threaded Binary Search Tree/bstTree.java
+++ Threaded Binary Search Tree/bstTree.java
@@ -52,7 +52,6 @@ class TBST extends BST
 	node.data = n;
         cur = root;
         pcur = null;
-
 	while(cur!=null && isThread == false)
 	{
 	    if(n < cur.data)
@@ -146,74 +145,71 @@ class TBST extends BST
 	        delete(temp.data);
 	        cur.data=d;
 	    }
-		        else if((cur.lt == true && cur.rt==true)||(cur.lt==true && cur.right==null)||(cur.rt==true&&cur.left==null))     // deleting a leaf
-		        {
-	              if(pcur.left == cur)
-            	  {
- 	                  pcur.left = cur.left;
-		    		        pcur.lt = true;
+	    else if((cur.lt == true && cur.rt==true)||(cur.lt==true && cur.right==null)||(cur.rt==true&&cur.left==null))     // deleting a leaf
+	    {
+	        if(pcur.left == cur)
+            	{
+ 	            pcur.left = cur.left;
+		    pcur.lt = true;
                 }
                 else
-            	  {
- 	                  pcur.right = cur.right;
-				            pcur.rt = true;
+                {
+ 	            pcur.right = cur.right;
+		    pcur.rt = true;
                 }
-		        }
+            }
             else if(cur.left != null && cur.lt == false) // deleting a node with a left child
-			      {
+	    {
                 temp = cur.left;
                 ptemp = cur;
-
-				        while(temp.right != null && temp.rt == false)
-				        {
-				            ptemp = temp;
-				            temp = temp.right;
-			          }
-			          if(cur == root)
-			          {
-					          root = cur.left;
-			              root.rt=false;
-			              root.right=null;
-			          }
-			          else
-			          {
-			              temp.right = cur.right;
-			              temp.rt = cur.rt;
-			              ptemp = cur.left;
-			              if(pcur.right == cur)
-			                  pcur.right = ptemp;
-			              else
-			                  pcur.left = ptemp;
+                while(temp.right != null && temp.rt == false)
+		{
+		    ptemp = temp;
+		    temp = temp.right;
+		}
+		if(cur == root)
+		{
+		    root = cur.left;
+		    root.rt = false;
+		    root.right = null;
+	        }
+		else
+		{
+		    temp.right = cur.right;
+		    temp.rt = cur.rt;
+		    ptemp = cur.left;
+		    if(pcur.right == cur)
+	                pcur.right = ptemp;
+		    else
+			pcur.left = ptemp;
                 }
-			      }
-			      else if(cur.right != null && cur.rt == false) // deleting a node with a right child
-			      {
+            }
+	    else if(cur.right != null && cur.rt == false) // deleting a node with a right child
+	    {
                 temp = cur.right;
                 ptemp = cur;
-
-				        while(temp.left != null && temp.lt == false)
-				        {
-				            ptemp = temp;
-				            temp = temp.left;
-			          }
-
-			          if(cur == root)
-			          {
-			              root = cur.right;
-			              root.lt=false;
-			              root.left=null;
-			          }
-			          else
-			          {
-			              temp.left = cur.left;
-			              temp.lt = cur.lt;
-			              ptemp = cur.right;
-			              if(pcur.left == cur)
-			                  pcur.left = ptemp;
-			              else
-			                  pcur.right = ptemp;
+                while(temp.left != null && temp.lt == false)
+		{
+		    ptemp = temp;
+		    temp = temp.left;
+		}
+		if(cur == root)
+		{
+		    root = cur.right;
+		    root.lt = false;
+		    root.left=null;
+	        }
+		else
+		{
+		    temp.left = cur.left;
+		    temp.lt = cur.lt;
+		    ptemp = cur.right;
+		    if(pcur.left == cur)
+		        pcur.left = ptemp;
+		    else
+			pcur.right = ptemp;
                 }
-			      }
+            }
         }
     }
 }
