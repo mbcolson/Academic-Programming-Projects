85385b2ae56372b567293ce0a907c7f81d66a371
Michael Colson <mbcolson@yahoo.com>
2017-10-01 08:05:03 -0700

    Update List.cpp

--- Linked List/List.cpp
+++ Linked List/List.cpp
@@ -4,14 +4,14 @@ using namespace std;
 //Post: an empty List is created && EndOfLIst()
 List::List(void)
 {
-     head = cur = NULL;     
+     head = cur = NULL;
 }
 
 //Post: A List is destroyed.
 List::~List(void)
 {
      Reset();
-                 
+
      while(!IsEmpty())
           Delete();
 }
@@ -19,22 +19,22 @@ List::~List(void)
 //Post: FCTVEL == The List is empty
 bool List::IsEmpty(void) const
 {
-     return head == NULL;     
+     return head == NULL;
 }
-    
+
 //Pre: !IsEmpty()
 //Post: List cursor is at the front of the list
 void List::Reset(void)
 {
-     cur = head;           
+     cur = head;
 }
-     
+
 //Post: FUCTVAL == The List cursor is beyond the last item
 bool List::EndOfList(void) const
 {
-     return cur == NULL;           
+     return cur == NULL;
 }
-     
+
 //Pre: !IsEmpty() && !EndOfList()
 //Post: List cursor is advanced to the next item
 void List::Advance(void)
@@ -43,27 +43,34 @@ void List::Advance(void)
 }
 
 //Pre: !IsEmpty() && !EndOfList()
-//Post: FCTVAL == Item at List cursor     
+//Post: FCTVAL == Item at List cursor
 ItemType List::CurrentItem(void)
 {
-     return cur->It;          
+     return cur->It;
 }
-    
-//Pre: !IsEmpty() && !EndOfList()
+
+//Pre: !IsEmpty()
 /*Post: Item at List cursor is deleted && the List cursor points
-        to the successor of deleted item */
+        to the successor of deleted item. If the List cursor points
+        to NULL after deletion and the list is not empty, then the
+        List cursor is reset to the head position. */
 void List::Delete()
 {
+	 if(IsEmpty())
+	 {
+		 cout << "Error: list is empty, cannot delete" << endl;
+		 return;
+	 }
      node *temp = new node;
      temp = head;
-     
+
      if(head == cur)
      {
           head = head->next;
           delete cur;
           cur = head;
      }
-     else 
+     else
      {
           while(temp->next != cur)
                temp = temp->next;
@@ -71,6 +78,8 @@ void List::Delete()
           delete cur;
           cur = temp->next;
      }
+     if(cur == NULL && !IsEmpty())
+         Reset();
 }
 
 /*Post: If the list was empty then Inserted is the only item in
@@ -82,11 +91,13 @@ void List::Insert(const ItemType& Inserted)
      node* temp = new node;
      temp->It = Inserted;
      temp->next = cur;
-     if(head==cur)
+     if(head == cur)
      {
           head = temp;
           cur = head;
-     } else {
+     }
+     else
+     {
           cur = head;
           while(cur->next != temp->next)
                cur = cur->next;
@@ -95,17 +106,20 @@ void List::Insert(const ItemType& Inserted)
      }
 }
 
-/*Post: List is displayed to standard out. This is used for
-        debugging only. */     
+//Post: List is displayed to standard out.
 void List::Display(void) const
 {
      node *temp;
      temp = head;
-     cout << "cur->It is: " << cur->It << endl;
+     if(cur != NULL)
+         cout << "cur->It is: " << cur->It << endl;
+     else
+    	 cout << "cur->It is: NULL" << endl;
      while(temp != NULL)
      {
           cout << temp->It << " -> ";
           temp = temp->next;
      }
      cout << "NULL" << endl;
+}
 }
