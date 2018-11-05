68e9e263244c394cac709838494fab59feb46b8f
Michael Colson <mbcolson@yahoo.com>
2018-11-04 17:05:20 -0800

    Update avlTest.java

--- AVL Tree/avlTest.java
+++ AVL Tree/avlTest.java
@@ -12,26 +12,40 @@ loop:	while(true)
 	    System.out.println("1: Insert ");  System.out.println("2: Delete ");
 	    System.out.println("3: exit");  System.out.print("Choice: "); System.out.flush();
 	    int cnt = System.in.read(buf); String s = new String(buf, 0, cnt-2);
+	
             if(!s.equals(""))
                 i = Integer.valueOf(s).intValue();  
             else 
                 continue;
+	
 	    switch(i) 
             {
-	        case 1: System.out.print("Number to be inserted: "); System.out.flush();
-		        cnt = System.in.read(buf); s = new String(buf, 0, cnt-2);
+	        case 1: System.out.print("Number to be inserted: "); 
+			System.out.flush();
+		        cnt = System.in.read(buf); 
+			s = new String(buf, 0, cnt-2);
+			    
          	        if(!s.equals("")) 
                             i = Integer.valueOf(s).intValue();  
                         else 
                             continue;
-			T.insert(i); T.draw(); continue;
-		case 2: System.out.print("Number to be deleted: "); System.out.flush();
-		        cnt = System.in.read(buf); s = new String(buf, 0, cnt-2);
-         	        if(!s.equals("")) 
+			    
+			T.insert(i); 
+			T.draw(); 
+			continue;
+		case 2: System.out.print("Number to be deleted: "); 
+			System.out.flush();
+		        cnt = System.in.read(buf); 
+			s = new String(buf, 0, cnt-2);
+         	        
+			if(!s.equals("")) 
                             i = Integer.valueOf(s).intValue(); 
                         else 
                             continue;
-			T.delete(i); T.draw(); continue;
+			    
+			T.delete(i); 
+			T.draw(); 
+			continue;
                 case 3: break loop;
 	    }
 	}
