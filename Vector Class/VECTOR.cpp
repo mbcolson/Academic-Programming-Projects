14056bb9f2eb085c1eb80412a4417614cb655819
Michael Colson <mbcolson@yahoo.com>
2019-12-18 22:43:31 -0800

    Update VECTOR.cpp

--- Vector Class/VECTOR.cpp
+++ Vector Class/VECTOR.cpp
@@ -11,14 +11,19 @@ VECTOR::VECTOR(long double x, long double y, long double z)
 
 long double VECTOR::dotProduct(const VECTOR &v) const
 {
-    return (this->x * v.x) + (this->y * v.y) + (this->z * v.z);
+     return (this->x * v.x) + (this->y * v.y) + (this->z * v.z);
 }
 
 const VECTOR VECTOR::crossProduct(const VECTOR &v) const
 {
-    return VECTOR((this->y * v.z) - (this->z * v.y),
-                 (this->z * v.x) - (this->x * v.z),
-                 (this->x * v.y) - (this->y * v.x));
+	return VECTOR((this->y * v.z) - (this->z * v.y),
+			(this->z * v.x) - (this->x * v.z),
+			(this->x * v.y) - (this->y * v.x));
+}
+
+const VECTOR VECTOR::scalarMultiply(long double s) const
+{
+	return VECTOR(this->x * s, this->y * s, this->z * s);
 }
 
 long double VECTOR::length() const
@@ -32,19 +37,19 @@ long double VECTOR::angle(const VECTOR &v) const
     return acos(this->dotProduct(v) / (this->length() * v.length()));
 }
 
-const VECTOR VECTOR::add(const VECTOR &v) const
+VECTOR operator +(const VECTOR &v1, const VECTOR &v2)
 {
-    return VECTOR(this->x + v.x, this->y + v.y, this->z + v.z);
+	return VECTOR(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
 }
 
-const VECTOR VECTOR::subtract(const VECTOR &v) const
+VECTOR operator -(const VECTOR &v1, const VECTOR &v2)
 {
-    return VECTOR(this->x - v.x, this->y - v.y, this->z - v.z);
+	return VECTOR(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
 }
 
-ostream &operator<<(ostream &output, const VECTOR &v)
+ostream &operator <<(ostream &output, const VECTOR &v)
 {
-    output << "(" << v.x << ", " << v.y << ", " << v.z << ")";
+     output << "(" << v.x << ", " << v.y << ", " << v.z << ")";
 
-    return output;
+     return output;
 }
