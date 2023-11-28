43017342fcd5d1db04db1e798b69176f630755c4
Michael Colson <mbcolson@yahoo.com>
2023-11-28 14:54:27 -0800

    Update Bouncing_Ball.java

--- Bouncing_Ball/Bouncing_Ball.java
+++ Bouncing_Ball/Bouncing_Ball.java
@@ -3,49 +3,45 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
 
-public class Bouncing_Ball extends JFrame
-{
+public class Bouncing_Ball extends JFrame {
     private float dx;
     private float dy;
     private int x_location;
     private int y_location;
 
-    public static void main(String args[]) throws Exception
-    {
+    public static void main(String args[]) throws Exception {
         Bouncing_Ball ballObj = new Bouncing_Ball(1,5,0,0);
         
-        for(int i = 0; i < 10000; i++)
-        {
+        for(int i = 0; i < 10000; i++) {
     	    ballObj.moveBall();
 
-    	    if(ballObj.x_location >= 425)
+    	    if (ballObj.x_location >= 425)
     	        ballObj.swapSignOfX();
 
-    	    if(ballObj.y_location >= 425)
+    	    if (ballObj.y_location >= 425)
                 ballObj.swapSignOfY();
 
-            if(ballObj.y_location == 0)
+            if (ballObj.y_location == 0)
                 ballObj.swapSignOfY();
 
-            if(ballObj.x_location == 0)
+            if (ballObj.x_location == 0)
                 ballObj.swapSignOfX();
 
-            if(ballObj.y_location == 135 && (ballObj.x_location >= 140 &&
+            if (ballObj.y_location == 135 && (ballObj.x_location >= 140 &&
                 ballObj.x_location <= 260) && ballObj.dy > 0)
                 ballObj.swapSignOfY();
 
-            if(ballObj.y_location == 260 && (ballObj.x_location >= 140 &&
+            if (ballObj.y_location == 260 && (ballObj.x_location >= 140 &&
                 ballObj.x_location <= 260) && ballObj.dy < 0)
                 ballObj.swapSignOfY();
 
-            if(ballObj.y_location == 145 && (ballObj.x_location >= 125 &&
+            if (ballObj.y_location == 145 && (ballObj.x_location >= 125 &&
                 ballObj.x_location <= 275) && ballObj.dy > 0)
                 ballObj.swapSignOfX();
         }
     }
 
-    public Bouncing_Ball(int dx, int dy, int x_location, int y_location)
-    {
+    public Bouncing_Ball(int dx, int dy, int x_location, int y_location) {
         setSize(500,500);
         setVisible(true);
         this.dx = dx;
@@ -55,8 +51,7 @@ public class Bouncing_Ball extends JFrame
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    public void paint(Graphics g)
-    {
+    public void paint(Graphics g) {
         g.clearRect(0,0,500,500);
         g.setColor(Color.red);
         g.fillOval(x_location,y_location,70,70);
@@ -64,21 +59,18 @@ public class Bouncing_Ball extends JFrame
         g.fillRect(200,200, 70,70);
     }
 
-    public void moveBall() throws Exception
-    {
+    public void moveBall() throws Exception {
         x_location += dx;
         y_location += dy; 
         Thread.sleep(8);
         repaint();
     }
 
-    public void swapSignOfX()
-    {
+    public void swapSignOfX() {
         dx = -dx;
     }
     
-    public void swapSignOfY()
-    {
+    public void swapSignOfY() {
         dy = -dy;
     }
 }
