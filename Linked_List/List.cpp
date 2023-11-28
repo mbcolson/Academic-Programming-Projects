1b0efb0e8f86a8b64d1a55e08be8cbb124387736
Michael Colson <mbcolson@yahoo.com>
2023-11-28 15:06:46 -0800

    Update List.cpp

--- Linked_List/List.cpp
+++ Linked_List/List.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 
 //Post: an empty List is created && EndOfLIst()
-List::List(void)
-{
+List::List(void) {
      head = cur = NULL;
 }
 
 //Post: A List is destroyed.
-List::~List(void)
-{
+List::~List(void) {
      Reset();
 
      while(!IsEmpty())
@@ -17,35 +15,30 @@ List::~List(void)
 }
 
 //Post: FCTVEL == The List is empty
-bool List::IsEmpty(void) const
-{
+bool List::IsEmpty(void) const {
      return head == NULL;
 }
 
 //Pre: !IsEmpty()
 //Post: List cursor is at the front of the list
-void List::Reset(void)
-{
+void List::Reset(void) {
      cur = head;
 }
 
 //Post: FUCTVAL == The List cursor is beyond the last item
-bool List::EndOfList(void) const
-{
+bool List::EndOfList(void) const {
      return cur == NULL;
 }
 
 //Pre: !IsEmpty() && !EndOfList()
 //Post: List cursor is advanced to the next item
-void List::Advance(void)
-{
+void List::Advance(void) {
      cur = cur->next;
 }
 
 //Pre: !IsEmpty() && !EndOfList()
 //Post: FCTVAL == Item at List cursor
-ItemType List::CurrentItem(void)
-{
+ItemType List::CurrentItem(void) {
      return cur->It;
 }
 
@@ -54,10 +47,8 @@ ItemType List::CurrentItem(void)
         to the successor of deleted item. If the List cursor points
         to NULL after deletion and the list is not empty, then the
         List cursor is reset to the head position. */
-void List::Delete()
-{
-     if(IsEmpty())
-     {
+void List::Delete() {
+     if (IsEmpty()) {
          cout << "Error: list is empty, cannot delete" << endl;
          return;
      }
@@ -65,14 +56,11 @@ void List::Delete()
      node *temp = new node;
      temp = head;
 
-     if(head == cur)
-     {
+     if (head == cur) {
           head = head->next;
           delete cur;
           cur = head;
-     }
-     else
-     {
+     } else {
           while(temp->next != cur)
                temp = temp->next;
           
@@ -81,7 +69,7 @@ void List::Delete()
           cur = temp->next;
      }
      
-     if(EndOfList() && !IsEmpty())
+     if (EndOfList() && !IsEmpty())
          Reset();
 }
 
@@ -89,19 +77,15 @@ void List::Delete()
         the list. Other wise, Inserted is the predecessor of the item
         that was current when the function was called and Inserted is
         the new current item. */
-void List::Insert(const ItemType& Inserted)
-{
+void List::Insert(const ItemType& Inserted) {
      node* temp = new node;
      temp->It = Inserted;
      temp->next = cur;
      
-     if(head == cur)
-     {
+     if(head == cur) {
           head = temp;
           cur = head;
-     }
-     else
-     {
+     } else {
           cur = head;
           
           while(cur->next != temp->next)
@@ -113,18 +97,16 @@ void List::Insert(const ItemType& Inserted)
 }
 
 //Post: List is displayed to standard out.
-void List::Display(void) const
-{
+void List::Display(void) const {
      node *temp;
      temp = head;
      
-     if(cur != NULL)
+     if (cur != NULL)
          cout << "cur->It is: " << cur->It << endl;
      else
          cout << "cur->It is: NULL" << endl;
      
-     while(temp != NULL)
-     {
+     while(temp != NULL) {
           cout << temp->It << " -> ";
           temp = temp->next;
      }
