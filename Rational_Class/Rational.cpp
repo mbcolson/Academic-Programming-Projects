a2bed81d0bdd7d8c4ab686d38c9c72a73d82f733
Michael Colson <mbcolson@yahoo.com>
2023-11-28 15:40:43 -0800

    Update Rational.cpp

--- Rational_Class/Rational.cpp
+++ Rational_Class/Rational.cpp
@@ -2,37 +2,32 @@
 using namespace std;
 
 //default constructor
-Rational::Rational()
-{
+Rational::Rational() {
      _p = 0;
      _q = 1;
 }
 
 // Constructor to initialize the rational object
-Rational::Rational(long p, long q)
-{
+Rational::Rational(long p, long q) {
      _p = p;  // numerator
      _q = q;  // denominator
      reduce();
 }
 
 // Copy constructor
-Rational::Rational(const Rational &src)
-{
+Rational::Rational(const Rational &src) {
      _p = src._p;
      _q = src._q;
 }
 
-Rational& Rational::operator=(const Rational &src)
-{
+Rational& Rational::operator=(const Rational &src) {
      _p = src._p;
      _q = src._q;
      reduce();
      return *this;
 }
 
-Rational& Rational::operator+=(const Rational &other)
-{
+Rational& Rational::operator+=(const Rational &other) {
      Rational temp;
      temp._p = _p;
      temp._q = _q;
@@ -45,8 +40,7 @@ Rational& Rational::operator+=(const Rational &other)
      return *this;
 }
 
-Rational& Rational::operator-=(const Rational &other)
-{
+Rational& Rational::operator-=(const Rational &other) {
      Rational temp;
      temp._p = _p;
      temp._q = _q;
@@ -59,8 +53,7 @@ Rational& Rational::operator-=(const Rational &other)
      return *this;
 }
 
-Rational& Rational::operator*=(const Rational &other)
-{
+Rational& Rational::operator*=(const Rational &other) {
      Rational temp;
      temp._p = _p;
      temp._q = _q;
@@ -70,8 +63,7 @@ Rational& Rational::operator*=(const Rational &other)
      return *this;
 }
 
-Rational& Rational::operator/=(const Rational &other)
-{
+Rational& Rational::operator/=(const Rational &other) {
      Rational temp;
      temp._p = _p;
      temp._q = _q;
@@ -84,22 +76,19 @@ Rational& Rational::operator/=(const Rational &other)
      return *this;
 }
 
-ostream& operator<< (ostream& os, const Rational &other)
-{
+ostream& operator<< (ostream& os, const Rational &other) {
      os << other._p << '/' << other._q;
      return os;
 }
 
-istream& operator>> (istream& is, Rational &other)
-{
+istream& operator>> (istream& is, Rational &other) {
      char s;
      is >> other._p >> s >> other._q;
      other.reduce();
      return is;
 }
 
-Rational Rational::operator+ (const Rational &other) const
-{
+Rational Rational::operator+ (const Rational &other) const {
      Rational temp;
      temp._p = _p;
      temp._q = _q;
@@ -107,8 +96,7 @@ Rational Rational::operator+ (const Rational &other) const
      return temp;
 }
 
-Rational Rational::operator+ (long l) const
-{
+Rational Rational::operator+ (long l) const {
      Rational temp;
      temp._p = _p;
      temp._q = _q;
@@ -116,15 +104,13 @@ Rational Rational::operator+ (long l) const
      return temp;
 }
 
-Rational operator+ (long l, const Rational &other)
-{
+Rational operator+ (long l, const Rational &other) {
      Rational temp = Rational(l);
      temp += other;
      return temp;
 }
 
-Rational Rational::operator- (const Rational& other) const
-{
+Rational Rational::operator- (const Rational& other) const {
      Rational temp;
      temp._p = _p;
      temp._q = _q;
@@ -132,8 +118,7 @@ Rational Rational::operator- (const Rational& other) const
      return temp;
 }
 
-Rational Rational::operator- (long l) const
-{
+Rational Rational::operator- (long l) const {
      Rational temp;
      temp._p = _p;
      temp._q = _q;
@@ -141,15 +126,13 @@ Rational Rational::operator- (long l) const
      return temp;
 }
 
-Rational operator- (long l, const Rational& other)
-{
+Rational operator- (long l, const Rational& other) {
      Rational temp = Rational(l);
      temp -= other;
      return temp;
 }
 
-Rational Rational::operator* (const Rational& other) const
-{
+Rational Rational::operator* (const Rational& other) const {
      Rational temp;
      temp._p = _p;
      temp._q = _q;
@@ -157,23 +140,20 @@ Rational Rational::operator* (const Rational& other) const
      return temp;
 }
 
-Rational Rational::operator* (long l) const
-{
+Rational Rational::operator* (long l) const {
      Rational temp;
      temp._p = _p;
      temp *= Rational(l);
      return temp;
 }
 
-Rational operator* (long l, const Rational& other)
-{
+Rational operator* (long l, const Rational& other) {
      Rational temp = Rational(l);
      temp *= other;
      return temp;
 }
 
-Rational Rational::operator/ (const Rational& other) const
-{
+Rational Rational::operator/ (const Rational& other) const {
      Rational temp;
      temp._p = _p;
      temp._q = _q;
@@ -181,8 +161,7 @@ Rational Rational::operator/ (const Rational& other) const
      return temp;
 }
 
-Rational Rational::operator/ (long l) const
-{
+Rational Rational::operator/ (long l) const {
      Rational temp;
      temp._p = _p;
      temp._q = _q;
@@ -190,181 +169,174 @@ Rational Rational::operator/ (long l) const
      return temp;
 }
 
-Rational operator/ (long l, const Rational& other)
-{
+Rational operator/ (long l, const Rational& other) {
      Rational temp = Rational(l);
      temp /= other;
      return temp;
 }
 
-Rational Rational::operator- () 
-{
+Rational Rational::operator- () {
      Rational &temp = *this;
      temp._p = -_p;
      return temp;
 }
 
-Rational Rational::operator+ () 
-{
+Rational Rational::operator+ () {
      Rational &temp = *this;
      return temp;
 }
 
-Rational Rational::operator++ (int) //post
-{
+Rational Rational::operator++ (int) { //post
      Rational &temp = *this;
      temp += 1;
      return temp;
 }
 
-Rational Rational::operator-- (int) //post
-{
+Rational Rational::operator-- (int) { //post
      Rational &temp = *this;
      temp -= 1;
      return temp;
 }
 
-Rational& Rational::operator++ () //pre
-{
+Rational& Rational::operator++ () { //pre
      Rational &temp = *this;
      temp += 1;
      return temp;
 }
 
-Rational& Rational::operator-- () //pre
-{
+Rational& Rational::operator-- () { //pre
      Rational &temp = *this;
      temp -= 1;
      return temp;
 }
 
-bool Rational::operator== (const Rational& other) const
-{
+bool Rational::operator== (const Rational& other) const {
      return(_p == other._p && _q == other._q);
 }
-bool Rational::operator== (long l) const 
-{
+
+bool Rational::operator== (long l) const {
      return(_p == l * _q);
 }
-bool operator== (long l, const Rational& other)
-{
+
+bool operator== (long l, const Rational& other) {
      return(other._p == l * other._q);
 }
-bool Rational::operator< (const Rational& other) const
-{
+
+bool Rational::operator< (const Rational& other) const {
      return (_p * other._q < _q * other._p);
 }
-bool Rational::operator< (long l) const  
-{
+
+bool Rational::operator< (long l) const {
      return (_p < l * _q);
 }
-bool operator< (long l, const Rational& other)
-{
+
+bool operator< (long l, const Rational& other) {
      return(l * other._q < other._p);
 }
-bool Rational::operator!= (const Rational& other) const
-{
+
+bool Rational::operator!= (const Rational& other) const {
      return (_p * other._q != _q * other._p); 
 }
-bool Rational::operator!= (long l) const  
-{
+
+bool Rational::operator!= (long l) const {
      return (_p != l * _q);
 }
-bool operator!= (long l, const Rational& other)
-{
+
+bool operator!= (long l, const Rational& other) {
      return (other._p != l * other._q);
 }
-bool Rational::operator> (const Rational& other) const
-{
+
+bool Rational::operator> (const Rational& other) const {
      return (_p * other._q > _q * other._p);
 }
-bool Rational::operator> (long l) const  
-{
+
+bool Rational::operator> (long l) const {
      return (_p > l * _q);
 }
-bool operator> (long l, const Rational& other)
-{
+
+bool operator> (long l, const Rational& other) {
      return (l * other._q > other._p);
 }
-bool Rational::operator<= (const Rational& other) const
-{
+
+bool Rational::operator<= (const Rational& other) const {
      return !(_p * other._q > _q * other._p);
 }
-bool Rational::operator<= (long l) const  
-{
+
+bool Rational::operator<= (long l) const {
      return !(_p > l * _q);
 }
-bool operator<= (long l, const Rational& other)
-{
+
+bool operator<= (long l, const Rational& other) {
      return !(l * other._q > other._p);
 }
-bool Rational::operator>= (const Rational& other) const
-{
+
+bool Rational::operator>= (const Rational& other) const {
      return !(_p * other._q < _q * other._p);
 }
-bool Rational::operator>= (long l) const  
-{
+
+bool Rational::operator>= (long l) const {
      return !(_p < l * _q);
 }
-bool operator>= (long l, const Rational& other)
-{
+
+bool operator>= (long l, const Rational& other) {
      return !(l * other._q < other._p);
 }
-Rational Rational::reciprocal ( ) const 
-{
+
+Rational Rational::reciprocal ( ) const {
      Rational temp;    
      temp._p = _q;
      temp._q = _p;
      return temp;
 }
-Rational Rational::pow(long L) const 
-{
+
+Rational Rational::pow(long L) const {
      Rational temp = *this; 
      Rational temp2;
      
-     if(L == 0)
+     if (L == 0)
           return 1;     
-     if(L < 0) 
-     {
+     
+     if (L < 0) {
           L *= -1;
           temp = reciprocal();
      }
+     
      temp2 = temp;
+     
      for(int i = 1; i < L; i++) 
           temp *= temp2;
+     
      return temp;
 }             
 
-Rational::operator long ( ) const 
-{
+Rational::operator long ( ) const {
      return (_p/_q);
 }
 
 /* Function: recursive function that takes two integers
              and finds the greatest common factor   */
-int Rational::gcf(int q1, int q2)
-{
-     if((q1 % q2 == 0) && q2 > 0)
+int Rational::gcf(int q1, int q2) {
+     if ((q1 % q2 == 0) && q2 > 0)
           return q2;
-     else if((q1 % q2 == 0) && q2 < 0)
+     else if ((q1 % q2 == 0) && q2 < 0)
           return -q2;
      else
           return gcf(q2, q1 % q2);
 }
 
 // Function: puts the rational number in reduced form
-void Rational::reduce()
-{
-     if(_q == 0)  // make sure the denominator is not 0
-     {
+void Rational::reduce() {
+     if (_q == 0) { // make sure the denominator is not 0
           _p = 0;
           _q = 1;
      }
-     if(_q < 0)  // if the denominator is a negative number,
-     {           // put the negative sign in the numerator
+     
+     if (_q < 0) { // if the denominator is a negative number,
+                   // put the negative sign in the numerator
           _p *= -1;
           _q *= -1;
      }
+     
      int n = gcf(_p, _q);  // reduce the rational number by
      _p = _p / n;          // dividing the numerator and
      _q = _q / n;          // denominator by the greatest
@@ -372,7 +344,6 @@ void Rational::reduce()
 
 /* Function: takes two integers and finds the lowest common
              multiple using the gcf function */
-int Rational::lcm(int q1, int q2)
-{
+int Rational::lcm(int q1, int q2) {
      return(q1 / gcf(q1, q2)) * q2;
 }
