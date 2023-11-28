763f8bb33ec736db68a6b037e2b036d65676a6be
Michael Colson <mbcolson@yahoo.com>
2023-11-28 15:58:52 -0800

    Update TicTacToe.java

--- Tic_Tac_Toe/TicTacToe.java
+++ Tic_Tac_Toe/TicTacToe.java
@@ -1,31 +1,26 @@
 import java.util.*;
 
-public class TicTacToe
-{
+public class TicTacToe {
     private char[][] gameBoard;
     private int turns, rowNum, colNum;
     private Scanner sc;
 
-    public TicTacToe()
-    {
+    public TicTacToe() {
         gameBoard = new char[3][3];
 	turns = 0;
         sc = new Scanner(System.in);
     }
 
-    private boolean isOpen(int row, int col)
-    {
+    private boolean isOpen(int row, int col) {
         return gameBoard[row][col] == ' ';
     }
 
-    private void addMove()
-    {
+    private void addMove() {
         gameBoard[rowNum][colNum] = whoseTurnIsIt(); 
 	turns++;
     }
 
-    private void displayBoard()
-    {
+    private void displayBoard() {
         System.out.println("\n  " + gameBoard[0][0] + " |  "
             + gameBoard[0][1] + " |  " + gameBoard[0][2]);
         System.out.println("--------------");
@@ -36,16 +31,14 @@ public class TicTacToe
             + gameBoard[2][1] + " |  " + gameBoard[2][2] + "\n");
     }
 
-    private char whoseTurnIsIt()
-    {
+    private char whoseTurnIsIt() {
         if(turns % 2 == 0)
             return 'O';
         else
             return 'X';
     }
 
-    private boolean isThereAWinner()
-    {
+    private boolean isThereAWinner() {
         return ((gameBoard[0][0] != ' ' && gameBoard[0][0] == gameBoard[1][1] &&
 	    gameBoard[1][1] == gameBoard[2][2]) || (gameBoard[2][0] != ' ' &&
 	    gameBoard[2][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[0][2]) ||
@@ -60,28 +53,23 @@ public class TicTacToe
             gameBoard[2][0] == gameBoard[2][1] && gameBoard[2][1] == gameBoard[2][2]));
     }
 
-    private void displayWinner()
-    {
-        if(whoseTurnIsIt() == 'X')
+    private void displayWinner() {
+        if (whoseTurnIsIt() == 'X')
             System.out.println("\nO is the Winner\n");
         else
             System.out.println("\nX is the Winner\n");
     }
 
-    private void initializeGame()
-    {
+    private void initializeGame() {
         for(int i = 0; i < 3; i++)
             for(int j = 0; j < 3; j++)
                 gameBoard[i][j] = ' ';
     }
 
-    private boolean isFull()
-    {
-        for(int i = 0; i < 3; i++)
-        { 
-            for(int j = 0; j < 3; j++)
-            {
-                if(gameBoard[i][j] == ' ')
+    private boolean isFull() {
+        for(int i = 0; i < 3; i++) { 
+            for(int j = 0; j < 3; j++) {
+                if (gameBoard[i][j] == ' ')
                     return false;
             }
         }
@@ -89,60 +77,53 @@ public class TicTacToe
         return true;
     }
 
-    private void getMove()
-    {
+    private void getMove() {
         boolean valid;
 
-        do
-        {
+        do {
             valid = true;
             System.out.printf("%c, choose the coordinates" +
                 " of your move: \nfirst row number (0,1 or 2)," +
                 "\nthen column number (0,1 or 2)\n", whoseTurnIsIt());
-            do
-            {
+            do {
                 valid = true;
                 rowNum = sc.nextInt();
 
-                if(rowNum < 0 || rowNum > 2) {
+                if (rowNum < 0 || rowNum > 2) {
                     valid = false;
                     System.out.print("\nInvalid row number, enter row number: "); 
                 }
             } while(!valid);
     
-            do
-            {
+            do {
                 valid = true;
                 colNum = sc.nextInt();
 
-                if(colNum < 0 || colNum > 2) {
+                if (colNum < 0 || colNum > 2) {
 		    valid = false;
                     System.out.print("\nInvalid column number, enter column number: ");
                 }
             } while(!valid);
 
-            if(!isOpen(rowNum, colNum))
-            {
+            if (!isOpen(rowNum, colNum)) {
                 valid = false;
                 System.out.println("\nThat position is already taken, try again");
             }
         } while(!valid);
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         TicTacToe game = new TicTacToe();
 
         game.initializeGame();
 
-        while(!game.isThereAWinner() && !game.isFull())
-        {
+        while(!game.isThereAWinner() && !game.isFull()) {
             game.getMove();
             game.addMove();
             game.displayBoard();
         }
 
-        if(!game.isThereAWinner())
+        if (!game.isThereAWinner())
             System.out.println("\nNo winner\n");
         else
             game.displayWinner();
