5e44a5883f0f80cee4c5ea08217b156508670b7d
Michael Colson <mbcolson@yahoo.com>
2023-11-28 01:37:33 -0800

    Update avlTest.java

--- AVL_Tree/avlTest.java
+++ AVL_Tree/avlTest.java
@@ -1,15 +1,12 @@
 import java.io.IOException;
 
-public class avlTest 
-{
-    public static void main(String[] args) throws IOException 
-    {
+public class avlTest {
+    public static void main(String[] args) throws IOException {
         int i, num = 0;
         AVL T = new AVL(); T.draw();
         byte [] buf = new byte[80];
         
-loop:   while(true) 
-        {
+loop:   while(true) {
             System.out.println("1: Insert ");  
             System.out.println("2: Delete ");
             System.out.println("3: exit");  
@@ -19,19 +16,18 @@ loop:   while(true)
             int cnt = System.in.read(buf); 
             String s = new String(buf, 0, cnt-2);
 	
-            if(!s.equals(""))
+            if (!s.equals(""))
                 i = Integer.valueOf(s).intValue();  
             else 
                 continue;
 	
-            switch(i) 
-            {
+            switch (i) {
                 case 1: System.out.print("Number to be inserted: "); 
                         System.out.flush();
                         cnt = System.in.read(buf); 
                         s = new String(buf, 0, cnt-2);
 		
-                        if(!s.equals("")) 
+                        if (!s.equals("")) 
                             i = Integer.valueOf(s).intValue();  
                         else 
                             continue;
@@ -44,7 +40,7 @@ loop:   while(true)
                         cnt = System.in.read(buf); 
                         s = new String(buf, 0, cnt-2);
          	        
-                        if(!s.equals("")) 
+                        if (!s.equals("")) 
                             i = Integer.valueOf(s).intValue(); 
                         else 
                             continue;
@@ -55,6 +51,6 @@ loop:   while(true)
                 case 3: break loop;
 	    }
 	}
-       System.exit(0);
+        System.exit(0);
     }
 }
