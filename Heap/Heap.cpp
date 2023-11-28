2e9e33103457e019f4826e0724073bd429843ffa
Michael Colson <mbcolson@yahoo.com>
2023-11-28 15:00:37 -0800

    Update Heap.cpp

--- Heap/Heap.cpp
+++ Heap/Heap.cpp
@@ -1,37 +1,31 @@
 #include "Heap.h"
 using namespace std;
 
-Heap::Heap()
-{
+Heap::Heap() {
      heap_array = new Itemtype[MAX_ITEMS];
      length = 0;
 }
 
-Heap::~Heap()
-{
+Heap::~Heap() {
      delete [] heap_array;
 }
 
-void Heap::Insert(const Itemtype& It)
-{
-     if(IsFull())
+void Heap::Insert(const Itemtype& It) {
+     if (IsFull())
           cout << "Heap is full" << endl;
-     else
-     {
+     else {
           length++;
           heap_array[length - 1] = It;
           ReheapUp(0, length - 1);
      }
 }
 
-Itemtype Heap::Retrieve() 
-{
+Itemtype Heap::Retrieve() {
      Itemtype It;    
          
-     if(IsEmpty())
+     if (IsEmpty())
           cout << "Heap is empty" << endl;
-     else
-     {
+     else {
           It = heap_array[0];
           heap_array[0] = heap_array[length - 1];
           length--;
@@ -40,37 +34,32 @@ Itemtype Heap::Retrieve()
      return It;
 }
 
-bool Heap::IsEmpty() const
-{
+bool Heap::IsEmpty() const {
      return length == 0;
 }
 
-bool Heap::IsFull() const
-{
+bool Heap::IsFull() const {
      return length == MAX_ITEMS;
 }
 
-void Heap::ReheapDown(int root, int last)
-{
+void Heap::ReheapDown(int root, int last) {
      int maxChild, RC, LC;
      Itemtype temp;
      
      LC = (root * 2) + 1;
      RC = (root * 2) + 2;
      
-     if(LC <= last)
-     {
-          if(LC == last)
+     if (LC <= last) {
+          if (LC == last)
                maxChild = LC;
-          else
-          {
-               if(heap_array[LC] <= heap_array[RC])
+          else {
+               if (heap_array[LC] <= heap_array[RC])
                     maxChild = RC;
                else
                     maxChild = LC;
           }
-          if(heap_array[root] < heap_array[maxChild])
-          {
+          
+          if (heap_array[root] < heap_array[maxChild]) {
                temp = heap_array[root];
                heap_array[root] = heap_array[maxChild];
                heap_array[maxChild] = temp;             
@@ -79,17 +68,14 @@ void Heap::ReheapDown(int root, int last)
      }
 }
 
-void Heap::ReheapUp(int root, int last)
-{
+void Heap::ReheapUp(int root, int last) {
      int parent;
      Itemtype temp;
      
-     if(last > root)
-     {
+     if (last > root) {
           parent = (last - 1) / 2;
           
-          if(heap_array[parent] < heap_array[last])
-          {
+          if (heap_array[parent] < heap_array[last]) {
                temp = heap_array[parent];
                heap_array[parent] = heap_array[last];
                heap_array[last] = temp;               
