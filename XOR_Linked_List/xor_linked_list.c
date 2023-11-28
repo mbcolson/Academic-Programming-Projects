1b18c1f2d0a9e1d54241221660d58b58703bac4a
Michael Colson <mbcolson@yahoo.com>
2023-11-28 01:30:48 -0800

    Update xor_linked_list.c

--- XOR_Linked_List/xor_linked_list.c
+++ XOR_Linked_List/xor_linked_list.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node
-{
+struct node {
     int data;
     unsigned long link;
 };
 
-void insert(struct node **head, struct node **tail, int n)
-{
+void insert(struct node **head, struct node **tail, int n) {
     struct node *cur, *prv, *nxt, *tmp;    
 
     tmp = (struct node *)malloc(sizeof(struct node));
@@ -17,8 +15,7 @@ void insert(struct node **head, struct node **tail, int n)
     cur = *head;
     prv = NULL;
 
-    while((cur != NULL) && (cur->data < tmp->data))
-    {
+    while((cur != NULL) && (cur->data < tmp->data)) {
         nxt = (struct node *)(cur->link ^ (unsigned long)prv);
         prv = cur;
         cur = nxt;
@@ -37,17 +34,14 @@ void insert(struct node **head, struct node **tail, int n)
         *tail = tmp; 
 }    
 
-void delete(struct node **head, struct node **tail, int n)
-{
+void delete(struct node **head, struct node **tail, int n) {
     struct node *cur, *prv, *nxt;
 
     cur = *head;
     prv = NULL;
 
-    while(cur != NULL)
-    {
-        if(cur->data == n)
-        {
+    while(cur != NULL) {
+        if(cur->data == n) {
             nxt = (struct node *)(cur->link ^ (unsigned long)prv);
 
             if (prv != NULL)
@@ -63,11 +57,9 @@ void delete(struct node **head, struct node **tail, int n)
             free(cur);
           
             cur = nxt;
-        }
-	else if(cur->data > n)
+        } else if(cur->data > n)
 	    break;
-        else
-        {
+        else {
             nxt = (struct node *)(cur->link ^ (unsigned long)prv);
             prv = cur;
             cur = nxt;
@@ -75,16 +67,14 @@ void delete(struct node **head, struct node **tail, int n)
     }
 }
 
-void list(struct node *head)
-{
+void list(struct node *head) {
     struct node *cur, *prv, *nxt;
     int count = 0;
 
     prv = NULL;
     cur = head;
 
-    while(cur != NULL)
-    {
+    while(cur != NULL) {
         if(count % 10 == 0)
             printf("\n");
         
@@ -98,8 +88,7 @@ void list(struct node *head)
     printf("\n");    
 }
 
-int main()
-{
+int main() {
     int i, num;
     struct node *head, *tail;
 
@@ -112,8 +101,7 @@ int main()
 
     list(head);
 
-    while(1) 
-    {
+    while(1) {
         printf("Enter number to delete (-1 to exit): ");
         scanf("%d", &num);
 
